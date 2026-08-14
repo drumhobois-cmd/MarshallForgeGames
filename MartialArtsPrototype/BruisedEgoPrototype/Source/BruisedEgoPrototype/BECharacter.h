46646fb0ddf597ce4f6e54dee88f1293686dac2a
drumhobois-cmd <drumhobo.is@gmail.com>
2026-08-14 21:59:50 +1000

    Add Bruised Ego base character class

---
+++ MartialArtsPrototype/BruisedEgoPrototype/Source/BruisedEgoPrototype/BECharacter.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BECharacter.generated.h"
+
+UCLASS()
+class BRUISEDEGOPROTOTYPE_API ABECharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABECharacter();
+};
